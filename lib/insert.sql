395f7625740845694f2a4ad76833fb777efa5288
Sammy Akharaz <ketrixz@gmail.com>
2018-05-24 14:45:10 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,92 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("Helicopter car", "transport", 6000000, "January 15th 2018", "January 15th 2019");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("Helicopter boat", "transport", 7000000, "January 15th 2018", "January 15th 2019");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("Helicopter plane", "transport", 8000000, "January 15th 2018", "January 15th 2019");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("Helicopter drone", "transport", 500000, "January 15th 2018", "January 15th 2019");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("Helicopter godzilla", "transport", 600000000, "January 15th 2018", "January 15th 2019");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("Helicopter bird", "transport", 5000000, "January 15th 2018", "January 15th 2019");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("Helicopter monster truck", "transport", 9000000, "January 15th 2018", "January 15th 2019");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("Helicopter toilet", "transport", 1000000, "January 15th 2018", "January 15th 2019");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+('Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'),
+('My book on SQL', 'books', 20.00, '2013-01-01', '2014-01-01');
+
+INSERT INTO users (name, age)
+VALUES ("Jane", 25);
+INSERT INTO users (name, age)
+VALUES ("Janeth", 25);
+INSERT INTO users (name, age)
+VALUES ("January", 25);
+INSERT INTO users (name, age)
+VALUES ("Janel", 25);
+INSERT INTO users (name, age)
+VALUES ("Janet", 25);
+INSERT INTO users (name, age)
+VALUES ("Jamie", 25);
+INSERT INTO users (name, age)
+VALUES ("Jamey", 25);
+INSERT INTO users (name, age)
+VALUES ("James", 25);
+INSERT INTO users (name, age)
+VALUES ("Jam", 25);
+INSERT INTO users (name, age)
+VALUES ("Jameson", 25);
+INSERT INTO users (name, age)
+VALUES ("Jameston", 25);
+INSERT INTO users (name, age)
+VALUES ("Jamal", 25);
+INSERT INTO users (name, age)
+VALUES ("Jamalia", 25);
+INSERT INTO users (name, age)
+VALUES ("Janette", 25);
+INSERT INTO users (name, age)
+VALUES ("Janetta", 25);
+INSERT INTO users (name, age)
+VALUES ("Jack", 25);
+INSERT INTO users (name, age)
+VALUES ("Jacket", 25);
+INSERT INTO users (name, age)
+VALUES ("Jax", 25);
+INSERT INTO users (name, age)
+VALUES ("Jigsaw", 25);
+INSERT INTO users (name, age)
+VALUES ("Jay", 25);
+
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES
+(1, 10.00, 1, 2),
+(2, 20.00, 1, 3),
+(3, 40.00, 1, 4),
+(4, 50.00, 2, 3),
+(5, 10.00, 3, 2),
+(6, 20.00, 4, 4),
+(7, 40.00, 5, 10),
+(8, 60.00, 6, 10),
+(9, 50.00, 7, 9),
+(10, 700.00, 8, 8),
+(11, 1000.00, 8, 7),
+(12, 40.00, 9, 6),
+(13, 50.00, 9, 3),
+(14, 50.00, 10, 4),
+(15, 24.00, 12, 1),
+(16, 34.00, 11, 1),
+(17, 12.00, 13, 6),
+(18, 19.00, 14, 5),
+(19, 20.00, 15, 5),
+(20, 40.00, 16, 6),
+(21, 35.50, 17, 7),
+(22, 40.00, 18, 8),
+(23, 60.00, 19, 9),
+(24, 70.00, 20, 10),
+(25, 100.00, 20, 4),
+(26, 40.00, 19, 1),
+(27, 20.00, 18, 6),
+(28, 90.00, 17, 9),
+(29, 230.00, 16, 6),
+(30, 450.00, 15, 5);
